680529df522d93342255ed8cc53eaa12a113737a
Alure Akash Harishchandra <83151892+Akashalure37@users.noreply.github.com>
2025-10-30 00:18:38 +0530

    Update AdminServiceMethods.js

--- src/service/AdminServiceMethods.js
+++ src/service/AdminServiceMethods.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const ADMIN_API_BASE_URL = 'http://localhost:8080/admin';
+const ADMIN_API_BASE_URL = 'https://ask-your-doctor.onrender.com/admin';
 
 class AdminServiceMethods {
    
